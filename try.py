9f2f86865b9171abe98022de0e307ba05ee881e4
riju-jec <64796073+riju-jec@users.noreply.github.com>
2021-06-14 23:30:17 +0530

    Initial Commit

--- try.py
+++ try.py
@@ -1,14 +1,25 @@
-from flask import Flask, redirect, url_for, render_template
-
+from flask import Flask, redirect, url_for, render_template, request, send_file
+from flask import *
+from pdf_image import pdf2img
+# from pdf_image import pdf2img
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html", x=3)
+@app.route('/')
+def index():
+    return render_template('home.html')
 
-@app.route("/home")
-def test():
-    return render_template("home.html", x=2)
+@app.route('/converted',methods = ['GET', 'POST'])
+def convert():
+    global f1
+    fi = request.files['img']
+    f1 = fi.filename
+    fi.save(f1)
+    pdf2img(f1)
+    return render_template('converted.html')
 
-if __name__ == "__main__":
-	app.run(debug=True)
+@app.route('/download')
+def download():
+    filename = f1.split('.')[0]+'converted.pdf'
+    return send_file(filename,as_attachment=True)
+
+app.run(debug=True)
