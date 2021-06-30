8d944c328535aa334629ba765f9580e3ced57237
riju-jec <64796073+riju-jec@users.noreply.github.com>
2021-06-30 22:35:02 +0530

    Update

--- try.py
+++ try.py
@@ -1,39 +1,50 @@
-from flask import Flask, redirect, url_for, render_template, request, send_file
+import os
+from flask import Flask, redirect, url_for, render_template, request, send_file, url_for, flash, sessions
 from flask import *
-from flask_ngrok import run_with_ngrok
+# from flask_ngrok import run_with_ngrok
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
+from flask import send_from_directory
+
+UPLOAD_FOLDER = 'uploaded_file/'
 
 
+app = Flask(__name__,template_folder='C:/Users/rijju/Documents/GitHub/Flask/sutiHTML/')
+# run_with_ngrok(app)  # Start ngrok when app is run
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = "super secret key"
 
-app = Flask(__name__,template_folder='/content/Flask_website/sutiHTML/')
-run_with_ngrok(app)  # Start ngrok when app is run
-
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def index():
-    return render_template('sample.html')
-
-# @app.route('/converted',methods = ['GET', 'POST'])
-# def convert():
-#     global f1
-#     fi = request.files['img']
-#     f1 = fi.filename
-#     fi.save(f1)
-#     return render_template('sample.html')
-
-# @app.route('/download')
-# def download():
-#     filename = f1.split('.')[0]+'converted.pdf'
-#     return send_file(filename,as_attachment=True)
-
-
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
+    if request.method == 'POST':
       f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+      filename=secure_filename(f.filename)
+      if filename== '':
+          flash('No file selected')
+      if filename:
+          f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+          return redirect(url_for('download',filename=filename))
+    
+    return render_template('upload_page.html')
 
+
+
+@app.route("/download/<filename>", methods = ['GET'])
+def download(filename):
+    # return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
+    return render_template('download.html',value=filename)
+
+
+@app.route("/download_file/<filename>", methods= ['GET'])
+def download_file(filename):
+    # return "<h1>Successfully uploaded</h1>"
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename,as_attachment=True)
+
+
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
 
 
 app.run() 
